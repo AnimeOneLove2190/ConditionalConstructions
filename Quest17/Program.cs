202db2a9b86e50fe89471f39de3d3edb81abb85e
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-06-30 00:02:42 +0500

    ;jgf

---
+++ Quest17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Quest17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradeOne = int.Parse(Console.ReadLine());
+            switch (gradeOne)
+            {
+                case 5:
+                    Console.WriteLine("Отлично");
+                    break;
+                case 4:
+                    Console.WriteLine("Хорошо");
+                    break;
+                case 3:
+                    Console.WriteLine("Удовлетворительно");
+                    break;
+                case 2:
+                    Console.WriteLine("Неудовлетворительно");
+                    break;
+                case 1:
+                    Console.WriteLine("Неудовлетворительно");
+                    break;
+            }
+        }
+    }
+}
---
+++ Quest18/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Quest18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+            int numThree = int.Parse(Console.ReadLine());
+            int sortNumOne = numOne;
+            int sortNumTwo = numTwo;
+            int sortNumThree = numThree;
+            bool minCondResOne = (numOne <= numTwo) & (numOne <= numThree);
+            bool minCondResTwo = (numTwo <= numOne) & (numTwo <= numThree);
+            bool minCondResThree = (numThree <= numOne) & (numThree <= numTwo);
+            if (minCondResOne)
+            {
+                sortNumOne = numOne;
+            }
+            if (minCondResTwo)
+            {
+                sortNumOne = numTwo;
+            }
+            if (minCondResThree)
+            {
+                sortNumOne = numThree;
+            }
+            bool maxCondResOne = (numOne >= numTwo) & (numOne >= numThree);
+            bool maxCondResTwo = (numTwo >= numOne) & (numTwo >= numThree);
+            bool maxCondResThree = (numThree >= numOne) & (numThree >= numTwo);
+            if (maxCondResOne)
+            {
+                sortNumThree = numOne;
+            }
+            if (maxCondResTwo)
+            {
+                sortNumThree = numTwo;
+            }
+            if (maxCondResThree)
+            {
+                sortNumThree = numThree;
+            }
+            bool mdCondOne = (numOne < sortNumThree) & (numOne > sortNumOne);
+            bool mdCondTwo = (numTwo < sortNumThree) & (numTwo > sortNumOne);
+            bool mdCondThree = (numThree < sortNumThree) & (numThree > sortNumOne);
+            if (mdCondOne)
+            {
+                sortNumTwo = numOne;
+            }
+            if (mdCondTwo)
+            {
+                sortNumTwo = numTwo;
+            }
+            if (mdCondThree)
+            {
+                sortNumTwo = numThree;
+            }
+            string result = $"{sortNumOne} {sortNumTwo} {sortNumThree}";
+            Console.WriteLine(result);
+        }
+    }
+}
