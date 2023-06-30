985b45acd26ab9849250c665945bdb368b9d3e57
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-07-01 02:38:09 +0500

    А ларчик просто открывался, ну или я просто слишком тупой

---
+++ Quest19/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Quest19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = int.Parse(Console.ReadLine());
+            string gradeA = "A";
+            string gradeB = "B";
+            string gradeC = "C";
+            string gradeD = "D";
+            string gradeF = "F";
+            if ((score >= 90) & (score <= 100))
+            {
+                Console.WriteLine(gradeA);
+            }
+            if ((score >= 80) & (score <= 89))
+            {
+                Console.WriteLine(gradeB);
+            }
+            if ((score >= 70) & (score <= 79))
+            {
+                Console.WriteLine(gradeC);
+            }
+            if ((score >= 60) & (score <= 69))
+            {
+                Console.WriteLine(gradeD);
+            }
+            if ((score >= 0) & (score <= 59))
+            {
+                Console.WriteLine(gradeF);
+            }
+        }
+    }
+}
---
+++ Quest20/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Quest20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+            int numThree = int.Parse(Console.ReadLine());
+            int numFour = int.Parse(Console.ReadLine());
+            if (numOne > numTwo)
+            {
+                int numInter = numTwo;
+                numTwo = numOne;
+                numOne = numInter;
+            }
+            if (numTwo > numThree)
+            {
+                int numInter = numThree;
+                numThree = numTwo;
+                numTwo = numInter;
+            }
+            if (numThree > numFour)
+            {
+                int numInter = numFour;
+                numFour = numThree;
+                numThree = numInter;
+            }
+            if (numOne > numTwo)
+            {
+                int numInter = numTwo;
+                numTwo = numOne;
+                numOne = numInter;
+            }
+            if (numTwo > numThree)
+            {
+                int numInter = numThree;
+                numThree = numTwo;
+                numTwo = numInter;
+            }
+            if (numOne > numTwo)
+            {
+                int numInter = numTwo;
+                numTwo = numOne;
+                numOne = numInter;
+            }
+            Console.WriteLine($"{numOne} {numTwo} {numThree} {numFour}");
+        }
+    }
+}
